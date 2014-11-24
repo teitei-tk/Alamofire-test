cb2d1a230f65101219c2df3e7bedc03a14966da6
teitei-tk <teitei.tk@gmail.com>
2014-11-24 16:46:03 +0900

    add : SwiftyJSON

--- Alamofire-test/ViewController.swift
+++ Alamofire-test/ViewController.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+
+        let apiRequest:Request = Alamofire.request(.GET, "http://api.ihackernews.com/page")
+        apiRequest.response(){
+            request, response, data, error in
+            println(data)
+        }
+   }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
